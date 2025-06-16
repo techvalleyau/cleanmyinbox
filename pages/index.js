dcc444345a74f85e2e5f25fddb0e67a30c1dbe89
techvalleyau <techvalleyau@gmail.com>
2025-06-16 23:06:54 +1000

    Fix index.js to use Next.js Link for /login

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 // pages/index.js
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Welcome to CleanMyInbox</h1>
-      <p>Start by <a href="/login">logging in</a>.</p>
+      <p>
+        Start by{' '}
+        <Link href="/login">
+          <a>logging in</a>
+        </Link>.
+      </p>
     </div>
   );
 }
