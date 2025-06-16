0a41552183623afb73e23ce51b3da56578cdce56
techvalleyau <techvalleyau@gmail.com>
2025-06-16 23:19:12 +1000

    Added Gmail Read-Only scope to Google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/gmail.readonly",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
