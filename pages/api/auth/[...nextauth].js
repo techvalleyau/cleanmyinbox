1230cb2693f9cd91e9171b7ed6216cee6756c0d1
techvalleyau <techvalleyau@gmail.com>
2025-06-18 15:24:53 +1000

    nextauth changes truehost saved

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+  trustHost: true,  // <--- Important for Vercel hosting
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -16,10 +18,13 @@ export default NextAuth({
       },
     }),
   ],
+
   secret: process.env.NEXTAUTH_SECRET,
+
   session: {
     strategy: "jwt",
   },
+
   callbacks: {
     async jwt({ token, account }) {
       if (account?.access_token) {
