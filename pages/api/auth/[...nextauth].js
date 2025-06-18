5dd20e39efa5b83ed173dcbcc13d31a864d84575
techvalleyau <techvalleyau@gmail.com>
2025-06-18 15:17:16 +1000

    safely check if account?.access_token and account?.refresh_token exist

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,29 +3,29 @@ import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-GoogleProvider({
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  authorization: {
-    params: {
-      scope: "openid email profile https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.modify",
-      prompt: "consent",
-      access_type: "offline",
-      response_type: "code",
-    },
-  },
-}),
-
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.modify",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    async jwt({ token, account}) {
-      // On initial sign in
-      if (account) {
+    async jwt({ token, account }) {
+      if (account?.access_token) {
         token.accessToken = account.access_token;
+      }
+      if (account?.refresh_token) {
         token.refreshToken = account.refresh_token;
       }
       return token;
@@ -34,6 +34,6 @@ GoogleProvider({
       session.accessToken = token.accessToken;
       session.refreshToken = token.refreshToken;
       return session;
-    }
-  }
+    },
+  },
 });
